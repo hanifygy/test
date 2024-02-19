bf3346343ed789329b878565baf8efed7adca095
wanlia8082 <wanlia8082@LAPTOP-NL0Q0KR3>
2024-02-19 22:17:02 +0800

    first commit

---
+++ src/projecttogithub/ProjectToGithub.java
@@ -0,0 +1,17 @@
+
+package projecttogithub;
+
+/**
+ *
+ * @author wanlia8082
+ */
+public class ProjectToGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
